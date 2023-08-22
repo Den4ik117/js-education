c81e49f934b89ad82e67353498b82a0cc8223231
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-08-22 13:06:18 +0500

    Проверка пуша в develop

--- basics_1/app.js
+++ basics_1/app.js
@@ -85,6 +85,7 @@ submitBtn.onclick = function () {
 
 // Сделал чуть больше, чем требовалось! В вёрстке еще и умножения с делением не было)
 // Некоторое изменение
+// Еще одно изменение
 function controlDecimalPlaces(e) {
     const input = e.target
     if (input.value.indexOf(".") !== Number('-1')) {
