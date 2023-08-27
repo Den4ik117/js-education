630e432d2c458d597b0bee653624419d63f47ab4
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-08-27 20:34:39 +0500

    Добавляет базовую работу списка дел

--- todo-list/src/app.ts
+++ todo-list/src/app.ts
@@ -1,9 +1,48 @@
-import { Test } from './test.ts';
+import {Task, getTaskHTML} from "./task.ts";
 
-const rootElement: Element | null = document.querySelector('.root');
+const inputElem = document.querySelector('#input') as HTMLInputElement;
+const buttonElem = document.querySelector('#button') as HTMLButtonElement;
+const tasksElem = document.querySelector('#tasks') as HTMLUListElement;
 
-Test.setTime();
 
-console.log(rootElement);
+interface List<T> {
+    items: T[];
 
+    push(item: T): void;
+
+    pop(): T | undefined;
+}
+
+class TODOList implements List<Task> {
+    items: Task[];
+    tasksElem: HTMLUListElement;
+
+    constructor(items?: Task[]) {
+        this.items = items ? items : [];
+        this.tasksElem = tasksElem;
+    }
+
+    push(task: Task): void {
+        this.items.push(task);
+        const taskHTMLEl = getTaskHTML(task);
+        tasksElem.appendChild(taskHTMLEl);
+        // tasksElem.insertAdjacentHTML('beforeend', getTaskHTMLElement(task));
+    }
+
+    pop(): Task | undefined {
+        return this.items.pop();
+    }
+}
+
+const tasks = new TODOList();
+
+buttonElem.addEventListener('click', () => {
+    const title = inputElem.value;
+    if (!title)
+        return;
+    const newTask: Task = {
+        title: title
+    }
+    tasks.push(newTask);
+})
 
