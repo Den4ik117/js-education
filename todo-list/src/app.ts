a98a065481604ab0df627558a54d5e155be1081f
Denis Zagvozdin <gaz91.91@mail.ru>
2023-08-28 14:27:48 +0500

    Добавляет правки

--- todo-list/src/app.ts
+++ todo-list/src/app.ts
@@ -1,9 +1,8 @@
-import {Task, getTaskHTML} from "./task.ts";
+import {Task, getTaskHTML} from './task';
 
-const inputElem = document.querySelector('#input') as HTMLInputElement;
-const buttonElem = document.querySelector('#button') as HTMLButtonElement;
-const tasksElem = document.querySelector('#tasks') as HTMLUListElement;
-
+const inputElem : HTMLInputElement | null = document.querySelector('#input');
+const buttonElem: HTMLButtonElement | null = document.querySelector('#button');
+const tasksElem: HTMLUListElement | null = document.querySelector('#tasks');
 
 interface List<T> {
     items: T[];
@@ -15,38 +14,76 @@ interface List<T> {
 
 class TODOList implements List<Task> {
     items: Task[];
-    tasksElem: HTMLUListElement;
+    tasksElem: HTMLUListElement | null;
 
     constructor(items?: Task[]) {
         this.items = items ?? [];
         this.tasksElem = tasksElem;
     }
 
-
     push(task: Task): void {
         this.items.push(task);
-        const taskHTMLEl = getTaskHTML(task);
-        this.tasksElem.appendChild(taskHTMLEl);
+        // const taskHTMLEl = getTaskHTML(task);
+        this.rerender();
+        // this.tasksElem?.appendChild(taskHTMLEl);
         localStorage.setItem('tasks', JSON.stringify(this.items));
     }
 
     pop(): Task | undefined {
-        return this.items.pop();
+        this.items.pop();
+
+        this.rerender();
+
+        localStorage.setItem('tasks', JSON.stringify(this.items));
+
+        return undefined;
+    }
+
+    rerender(): void {
+        const str = this.items.map((item) => {
+            return `
+                <li class="list-group-item d-flex align-items-center justify-content-between mb-2 border rounded">
+                    <span>${item.title}</span>
+                    <button class="btn btn-danger button" data-title="${item.title}">Удалить</button>
+                </li>
+            `
+        }).join('');
+
+        if (this.tasksElem) {
+            this.tasksElem.innerHTML = str;
+        }
     }
 
     receiveStorageTasks(): void {
         const rawStorageTasks = localStorage.getItem('tasks');
         const storageTasks = rawStorageTasks ? JSON.parse(rawStorageTasks) as Task[] : [];
-        storageTasks.slice().reverse().forEach((task) => {
-           this.push(task);
-        });
+        // console.log(storageTasks);
+        this.items = storageTasks;
+        this.rerender();
+        // storageTasks.slice().reverse().forEach((task) => {
+        //    this.push(task);
+        // });
     }
 }
 
 const toDoList = new TODOList();
 
-buttonElem.addEventListener('click', () => {
-    const title = inputElem.value;
+tasksElem?.addEventListener('click', (event) => {
+    const element = event.target.closest('.button');
+
+    if (element) {
+        const { title } = element.dataset;
+
+        console.log(title);
+
+        // TODO найти с таким названием, удалить
+
+        toDoList.rerender();
+    }
+})
+
+buttonElem?.addEventListener('click', () => {
+    const title = inputElem?.value;
     if (!title)
         return;
     const newTask: Task = {
