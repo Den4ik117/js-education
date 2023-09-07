318f4e03b6f9aadff492d9a91faf5c9e0f9fe965
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-09-07 23:53:57 +0500

    Добавляет аргументы в функции, переименовывает локальные переменные

--- todo-list/src/scripts/task.ts
+++ todo-list/src/scripts/task.ts
@@ -2,36 +2,38 @@ export interface Task {
     title: string;
 }
 
-function createTaskTitleHTMLElem(title: string) {
-    const titleHTML: HTMLSpanElement = document.createElement('span');
-    titleHTML.textContent = title;
-    return titleHTML;
+function createTaskTitleHTMLElem(title: string, classNames?: string[]) {
+    const titleElem = document.createElement('span');
+    if (!classNames)
+        classNames = []
+    titleElem.textContent = title;
+    titleElem.className = classNames.join();
+    return titleElem;
 }
 
-function createTaskButtonHTMLElem(taskTitle: string): HTMLButtonElement {
-    const btnEl: HTMLButtonElement = document.createElement('button');
-    const btnClassNames = ['btn', 'btn-danger'];
-    btnEl.className = btnClassNames.join(' ');
-    btnEl.textContent = 'X';
+function createTaskButtonHTMLElem(taskTitle: string, classNames?: string[], textContent: string = 'X'): HTMLButtonElement {
+    const btnEl = document.createElement('button');
+    if (!classNames)
+        classNames = ['btn', 'btn-danger'];
+    btnEl.className = classNames.join(' ');
+    btnEl.textContent = textContent;
     btnEl.setAttribute('data-task-title', taskTitle);
-
     return btnEl;
 }
 
-function createTaskLIHTMLElem(title: HTMLSpanElement, btn: HTMLButtonElement): HTMLLIElement {
-    const liHTML: HTMLLIElement = document.createElement('li');
-    const liClassNames = ['list-group-item', 'd-flex', 'align-items-center', 'justify-content-between', 'mb-2',
-        'border', 'rounded'];
-    liHTML.className = liClassNames.join(' ');
-    liHTML.appendChild(title);
-    liHTML.appendChild(btn);
-
-    return liHTML;
+function createTaskLIHTMLElem(title: HTMLSpanElement, btn: HTMLButtonElement, classNames?: string[]): HTMLLIElement {
+    const liElem = document.createElement('li');
+    if (!classNames)
+        classNames = ['list-group-item', 'd-flex', 'align-items-center',
+            'justify-content-between', 'mb-2', 'border', 'rounded'];
+    liElem.className = classNames.join(' ');
+    liElem.appendChild(title);
+    liElem.appendChild(btn);
+    return liElem;
 }
 
 export function getTaskHTMLElem(task: Task): HTMLLIElement {
-    const titleHTML = createTaskTitleHTMLElem(task.title);
-    const btnHTML = createTaskButtonHTMLElem(task.title);
-
-    return createTaskLIHTMLElem(titleHTML, btnHTML);
+    const titleElem = createTaskTitleHTMLElem(task.title);
+    const btnElem = createTaskButtonHTMLElem(task.title);
+    return createTaskLIHTMLElem(titleElem, btnElem);
 }
